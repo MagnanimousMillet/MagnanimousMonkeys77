2394f82af10af61a4e574d2d718d83a2fad9ed57
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-07-06 11:52:29 -0700

    (feat) speech to text moved to Lecturebutton Component

--- react-client/src/components/LectureButtons.jsx
+++ react-client/src/components/LectureButtons.jsx
@@ -4,8 +4,63 @@ import axios from 'axios';
 class LectureButtons extends React.Component {
 	constructor (props) {
 		super(props);
-		this.state = {};
+    this.state = {
+      question: '',
+      hasMicrophone: false,
+      recording: false
+    }
+    this.recognition = '';
 	}
+
+  componentDidMount() {
+    this.setState({
+      hasMicrophone: this.hasGetUserMedia()
+    })
+  }
+
+  hasGetUserMedia() {
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  }
+
+  onRecord() {
+    this.setState({
+      recording: !this.state.recording,
+    }, () => {
+      this.state.recording ? this.startRecording() : this.stopRecording()
+    })
+  }
+
+  onChange(event) {
+  	this.setState({
+  		question: event.target.value
+  	})
+  }
+
+  stopRecording() {
+    this.recognition.onend = () => console.log('ended');
+    this.recognition.stop();
+  }
+
+  startRecording() {
+    this.setState({
+      question: ''
+    }, () => { 
+      var index = 0;
+      this.recognition = new webkitSpeechRecognition();
+      this.recognition.continuous = true;
+      this.recognition.interimResults = true;
+      this.recognition.start();
+      this.recognition.onstart = () => console.log('hello');
+      this.recognition.onspeechend = () => console.log('woah');
+      this.recognition.onresult = (event) => {
+        if (event.results[index].isFinal) {
+          this.setState({
+            question: this.state.question += event.results[index]['0'].transcript
+          }, () => {index++})
+        }
+      }
+    })
+  }
 
   onThumbsCheck () {
   	console.log('onThumbsCheck is being called');
@@ -27,6 +82,13 @@ class LectureButtons extends React.Component {
 		return (
 			<div className="row">
 				<div className="col-xs-12 text-center">
+					<input
+            type="text"
+            className="form-control"
+            value={this.state.question}
+            placeholder="Enter question..."
+            onChange={this.onChange.bind(this)}
+          />
 					<div
 						className="btn btn-lg btn-success"
 						onClick={this.onThumbsCheck.bind(this)}>
--- react-client/src/components/LectureStarter.jsx
+++ react-client/src/components/LectureStarter.jsx
@@ -1,84 +1,36 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 class LectureStarter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	name: '',
-      hasMicrophone: false,
-      recording: false
-    }
-    this.recognition = ''
-  }
-
-
-  hasGetUserMedia() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  }
-
-  onRecord() {
-    this.setState({
-      recording: !this.state.recording,
-    }, () => {
-      this.state.recording ? this.startRecording() : this.stopRecording()
-    })
-  }
-
-  startRecording() {
-    this.setState({
       name: ''
-    }, () => { 
-      var index = 0;
-      this.recognition = new webkitSpeechRecognition();
-      this.recognition.continuous = true;
-      this.recognition.interimResults = true;
-      this.recognition.start();
-      this.recognition.onstart = () => console.log('hello');
-      this.recognition.onspeechend = () => console.log('woah');
-      this.recognition.onresult = (event) => {
-        if (event.results[index].isFinal) {
-          this.setState({
-            name: this.state.name += event.results[index]['0'].transcript
-          }, () => {index++})
-        }
-      }
-    })
-  }
-
-  stopRecording() {
-    this.recognition.stop();
-    this.recognition.onend = () => console.log('ended');
-  }
-
-  componentDidMount() {
-    this.setState({
-      hasMicrophone: this.hasGetUserMedia()
-    })
+    };
   }
 
   handleChange (event) {
-  	this.setState({
-  		name: event.target.value
-  	});
+    this.setState({
+      name: event.target.value
+    });
   }
 
   onLectureStart () {
     axios({
-	    method: 'post',
-	    url: '/lecture',
-	    params: {
-	      name: this.state.name
-	    }
-	  }).then((response) => {
-  		this.props.startLecture(response.data.lectureId);
-	  }).catch((error) => {
-	  	console.log(error);
-	  })
+      method: 'post',
+      url: '/lecture',
+      params: {
+        name: this.state.name
+      }
+    }).then((response) => {
+      this.props.startLecture(response.data.lectureId);
+    }).catch((error) => {
+      console.log(error);
+    })
   }
 
-	render () {
-  	return (
+  render () {
+    return (
       <div className="row text-center">
         <div className="col-xs-12 text-center">
           <input
@@ -90,17 +42,15 @@ class LectureStarter extends React.Component {
           />
         </div>
         <div className="col-xs-12 text-center">
-        	<div
+          <div
             className="btn btn-lg btn-success"
             onClick={this.onLectureStart.bind(this)}>
             Start Lecture
           </div>
         </div>
-        {this.state.hasMicrophone ?
-          <img onClick={this.onRecord.bind(this)} className="btn btn-mic" src="ios-9-siri-icon-768x766.png"></img> : <div></div>}
       </div>
-  	)
-	}
+    )
+  }
 }
 
-export default LectureStarter
+export default LectureStarter;
